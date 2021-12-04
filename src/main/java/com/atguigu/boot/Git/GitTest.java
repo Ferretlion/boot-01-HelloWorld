992254fbf1498f1e9ed159abb940fbf628df0f11
FerretLion <1023744469@qq.com>
2021-12-04 19:46:23 +0800

    hot-fix first

--- src/main/java/com/atguigu/boot/Git/GitTest.java
+++ src/main/java/com/atguigu/boot/Git/GitTest.java
@@ -15,9 +15,6 @@ public class GitTest
     {
         System.out.println("你好");
         System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
+        System.out.println("hhhhhhhh");
     }
 }
