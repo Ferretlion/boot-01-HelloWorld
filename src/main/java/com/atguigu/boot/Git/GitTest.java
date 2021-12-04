4ded472bbb3534f88332e502476c46366f2578fd
FerretLion <1023744469@qq.com>
2021-12-04 21:08:11 +0800

    master gitee

--- src/main/java/com/atguigu/boot/Git/GitTest.java
+++ src/main/java/com/atguigu/boot/Git/GitTest.java
@@ -14,23 +14,7 @@ public class GitTest
     public static void main(String[] args)
     {
         System.out.println("master");
-        System.out.println("热修补");
-        System.out.println("热修补");
-        System.out.println("master");
-        System.out.println("master");
-        System.out.println("master");
-        System.out.println("master");
-        System.out.println("master");
-        System.out.println("zhaoxuanye");
-        System.out.println("zhaoxuanye111");
         System.out.println("******************");
         System.out.println("******************");
-        System.out.println("******************");
-        System.out.println("******************");
-        System.out.println("-----------------------");
-         System.out.println("-----------------------");
-         System.out.println("-----------------------");
-         System.out.println("-----------------------");
-         System.out.println("-----------------------");
     }
 }
