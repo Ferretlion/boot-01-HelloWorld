5a1834de31c558f9235668cb5ec40b59be45754a
FerretLion <1023744469@qq.com>
2021-12-04 19:57:38 +0800

    hot-fix*****

--- src/main/java/com/atguigu/boot/Git/GitTest.java
+++ src/main/java/com/atguigu/boot/Git/GitTest.java
@@ -13,7 +13,9 @@ public class GitTest
 {
     public static void main(String[] args)
     {
-        System.out.println("热修补");
-        System.out.println("热修补");
+        System.out.println("******************");
+        System.out.println("******************");
+        System.out.println("******************");
+        System.out.println("******************");
     }
 }
