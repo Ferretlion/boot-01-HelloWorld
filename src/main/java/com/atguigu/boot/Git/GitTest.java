0dc560963aa2434d296aa7254952a1e9e798f40c
FerretLion <1023744469@qq.com>
2021-12-04 19:41:07 +0800

    hello world forth

--- src/main/java/com/atguigu/boot/Git/GitTest.java
+++ src/main/java/com/atguigu/boot/Git/GitTest.java
@@ -14,5 +14,10 @@ public class GitTest
     public static void main(String[] args)
     {
         System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
     }
 }
